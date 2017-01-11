d999b7c3d4b8a5bdaf34de3fa839371cd3ccd5f9
K8161 <K8161@ICT-421-WS-13>
2017-01-11 14:28:29 +0200

    Refaktorointitehtava

--- BlackJackV0/BlackJackV0/BlackJack.cs
+++ BlackJackV0/BlackJackV0/BlackJack.cs
@@ -6,29 +6,45 @@ namespace JAMK.IT
 {
   class BlackJack
   {
-    static void Main()
-    {
-      int myNumber = 17;
-      int theirNumber;
-      System.Console.WriteLine("*** BlackJack! ***");
-      System.Console.Write("Can you beat my number? Enter any number between 1-21: ");
-      //reading and converting 
-      theirNumber = System.Convert.ToInt32(System.Console.ReadLine());
-      //comparing that given umber is valid
-      if (theirNumber < 1 || theirNumber > 21)
-      {
-        Console.WriteLine("The given number is out of limits, try again.");
-      }
-      else {
-        //comparing
-        if (theirNumber >= myNumber && theirNumber <= 21)
+        static void Main()
         {
-          System.Console.WriteLine("You win.");
-        }
-        else {
-          System.Console.WriteLine("You lose.");
-        }
+            int myNumber;
+            int theirNumber;
+            string ex = "empty";
+
+            Random rand = new Random();
+
+            System.Console.WriteLine("*** BlackJack! ***");
+           
+                System.Console.Write("Can you beat my number? Enter any number between 1-21: ");
+                //reading and converting 
+
+                ex = System.Console.ReadLine();
+            do
+            {
+                theirNumber = int.Parse(ex);
+                while (theirNumber < 1 || theirNumber > 21)
+                {
+                    System.Console.Write("Wrong number. Enter any number between 1-21: ");
+                    //reading and converting 
+
+                    theirNumber = System.Convert.ToInt32(System.Console.ReadLine());
+                }
+
+                //comparing that given umber is valid
+                myNumber = rand.Next(10, 21);
+
+                //comparing
+                if (theirNumber >= myNumber && theirNumber <= 21)
+                {
+                    System.Console.WriteLine("You win. My number was: " + myNumber);
+                }
+                else {
+                    System.Console.WriteLine("You lose. My number was: " + myNumber);
+                }
+                System.Console.WriteLine("Want to play again? Enter number between 1-21 or x/exit to quit game: ");
+                ex = System.Console.ReadLine();
+            } while (ex != "x" && ex != "exit");
       }
     }
   }
-}
